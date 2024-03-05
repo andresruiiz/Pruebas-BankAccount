9ae22713b3a4fe969f05fe8fc56ad1682a284605
andresruiiz <andresruiz@uma.es>
2024-03-05 11:30:06 +0100

    Create Maven project

---
+++ Ejercicio1BankAccountJUnit/src/test/java/BankAccountTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BankAccountTest {
+
+    @BeforeEach
+    void setup() {
+        BankAccount bankAccount = new BankAccount(100);
+    }
+
+    
+
+}
